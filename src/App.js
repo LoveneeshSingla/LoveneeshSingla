8d0bfe9c3faa46178464a35aa4b2b9f6089eeb02
deepak <loveneesh.zestgeek@gmail.com>
2021-10-07 18:19:52 +0530

    todo list creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-
-import './App.css';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import 'semantic-ui-css/semantic.min.css'
+import AddTodo from "./components/AddTodo";
+import Header from "./components/Header";
+import ShowTodo from "./components/ShowTodo";
+import { useState, useEffect } from 'react';
+// import uuid from "uuidv4"
 
 function App() {
+
+  const [TodoItems, setTodoItems] = useState([]);
+
+  const LOCAL_STORAGE_KEY = "todos";
+
+  useEffect(() => {
+    const retrieveTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (retrieveTodos)
+      setTodoItems(retrieveTodos);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(TodoItems));
+  }, [TodoItems])
+
+
+  const addTodoHandler = (todo) => {
+    setTodoItems([...TodoItems, todo])
+  }
+
   return (
-    <div className="App">
-      <h1>Hello to React World</h1>
-      <h2>Welcome to React App</h2>
+    <div>
+      <Header />
+      <AddTodo addTodoHandler={addTodoHandler} />
+      <ShowTodo TodoItems={TodoItems} />
     </div>
   );
 }
